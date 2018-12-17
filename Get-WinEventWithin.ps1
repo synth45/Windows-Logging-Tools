5096941e057fedf0bbccaf998b573bfa573fa260
synth45 <44142619+synth45@users.noreply.github.com>
2018-12-16 21:29:17 -0800

    Add files via upload

---
+++ Get-WinEventWithin.ps1
@@ -0,0 +1,17 @@
+﻿#This tool will search Windows log files based on following parameters 
+
+##Need to record variables that we will be working with. Script will reach out to computers based on timestamps
+
+param([string]$ComputerName = 'localhost',[datetime]$StartTimestamp,[datetime]$EndTimestamp)
+
+##Must store log data into a log variable for further manipulation
+
+###The where operator will serve to output only winevent objects that have a record count
+$Logs = (Get-WinEvent -ListLog * -ComputerName $ComputerName | where { $_.RecordCount }).Logname
+$FilterTable = @{
+    'StartTime' = $StartTimestamp
+    'EndTime' = $EndTimestamp
+    'LogName' = $Logs
+}
+
+Get-WinEvent -ComputerName $ComputerName -FilterHashtable $FilterTable -ErrorAction 'SilentlyContinue'
